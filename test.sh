8d98bfc7c38723b7edffdd6f8575988808643510
Jan Ryklikas <jry@facelift-bbt.com>
2022-08-17 23:51:13 +0200

    avahi dockerfile

--- test.sh
+++ test.sh
@@ -7,7 +7,8 @@ printf "        Please Select Operating System to test"
 printf "${HORIZONTAL_LINE}"
 printf "1) Alpine${NL}"
 printf "2) Debian${NL}"
-printf "3) Arch"
+printf "3) Arch${NL}"
+printf "4) Avahi"
 printf "${HORIZONTAL_LINE}"
 read -r os_int
 
@@ -26,6 +27,11 @@ case $os_int in
         docker rm arch_testing
         docker build -t arch_testing:latest -f Dockerfile.archlinux .
         docker run --name arch_testing -it arch_testing
+        ;;
+    "4")
+        docker rm avahi_testing
+        docker build -t avahi_testing:latest -f Dockerfile.avahi .
+        docker run --name avahi_testing -it avahi_testing
         ;;
     *)
         docker rm alpine_testing
